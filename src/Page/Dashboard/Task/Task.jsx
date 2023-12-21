3644fef5d79c291fc463153ee3d8228ff53cd923
Ruma543 <ruma.afrin543@gmail.com>
2023-12-21 20:16:55 +0600

    update task component

--- src/Page/Dashboard/Task/Task.jsx
+++ src/Page/Dashboard/Task/Task.jsx
@@ -14,16 +14,31 @@ import { MdDelete } from 'react-icons/md';
 
 const Task = () => {
   const [startDate, setStartDate] = useState(new Date());
+  const [tasks, setTasks] = useState([]);
   const { user } = useAuth();
   // step-2: dnd
   const handleDragEnd = result => {
     if (!result.destination) return;
 
     const { source, destination } = result;
+    const updatedTasks = [...tasks];
+    const movedTask = updatedTasks.splice(source.index, 1)[0];
+    movedTask.status = 'To-do';
 
-    // Implement logic to update task order in the corresponding lists
-    // Use the index from source and destination to reorder the tasks array
+    if (destination.droppableId === 'ongoing') {
+      movedTask.status = 'Ongoing';
+    } else if (destination.droppableId === 'complete') {
+      movedTask.status = 'Complete';
+    }
+
+    updatedTasks.splice(destination.index, 0, movedTask);
+    setTasks(updatedTasks);
   };
+  // const handleDragEnd = result => {
+  //   if (!result.destination) return;
+
+  //   const { source, destination } = result;
+  // };
   // step-3
   // const onDrop = (item, monitor) => {
   //   console.log(item);
@@ -116,96 +131,141 @@ const Task = () => {
     });
   };
   return (
-    // <DndProvider backend={HTML5Backend}>
     <div className="w-11/12 mx-auto">
-      <h3>Your submitted work is {taskData.length}</h3>
+      <h3 className="text-3xl font-semibold">
+        Your submitted work is {taskData.length}
+      </h3>
       {/* <DragDropContext onDragEnd={handleDragEnd}> add korchi */}
-      <DragDropContext onDragEnd={handleDragEnd}>
-        <div className="grid lg:grid-cols-2 grid-cols-1">
-          <div className="">
-            <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Title</span>
-                </label>
-                <input
-                  type="title"
-                  {...register('title', { required: true })}
-                  placeholder="enter your title"
-                  className="input input-bordered"
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Description</span>
-                </label>
-                <input
-                  type="description"
-                  {...register('description', { required: true })}
-                  placeholder="enter your description"
-                  className="input input-bordered"
-                />
-              </div>
+      {/* */}
+      <div className="lg:flex- flex-col">
+        <div className="">
+          <form
+            className="card-body flex lg:flex-row rounded-lg"
+            style={{ backgroundColor: '#0a3d62' }}
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-semibold text-white">
+                  Title
+                </span>
+              </label>
+              <input
+                type="title"
+                {...register('title', { required: true })}
+                placeholder="enter your title"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-semibold text-white">
+                  Description
+                </span>
+              </label>
+              <input
+                type="description"
+                {...register('description', { required: true })}
+                placeholder="enter your description"
+                className="input input-bordered"
+              />
+            </div>
 
-              <div className="form-control ">
-                <label className="label">
-                  <span className="label-text">Deadlines</span>
-                </label>
+            <div className="form-control ">
+              <label className="label">
+                <span className="label-text font-semibold text-white">
+                  Deadlines
+                </span>
+              </label>
 
-                <Controller
-                  control={control}
-                  name="date"
-                  className=""
-                  render={({ field }) => (
-                    <DatePicker
-                      {...field}
-                      selected={field.value}
-                      onChange={date => field.onChange(date)}
-                      minDate={new Date()}
-                      className="w-full rounded-lg px-4 py-2 outline-0"
-                    />
-                  )}
-                  rules={{ required: true }}
-                />
-              </div>
-              <div className="form-control ">
-                <label className="label">
-                  <span className="label-text">Priority</span>
-                </label>
-                <select
-                  {...register('priority', { required: true })}
-                  className=" w-full px-6 py-3 text-black rounded-lg"
-                  name="priority"
-                  id=""
-                  defaultValue="default"
-                >
-                  <option disabled value="default"></option>
-                  <option value="Low">Low</option>
-                  <option value="Moderate">Moderate </option>
-                  <option value="High">High</option>
-                </select>
-                {errors.occupation && (
-                  <span className="text-red-400">Priority is required</span>
+              <Controller
+                control={control}
+                name="date"
+                className=""
+                render={({ field }) => (
+                  <DatePicker
+                    {...field}
+                    selected={field.value}
+                    onChange={date => field.onChange(date)}
+                    minDate={new Date()}
+                    className="w-full rounded-lg px-4 py-3 outline-0"
+                  />
                 )}
-              </div>
+                rules={{ required: true }}
+              />
+            </div>
+            <div className="form-control ">
+              <label className="label">
+                <span className="label-text font-semibold text-white">
+                  Priority
+                </span>
+              </label>
+              <select
+                {...register('priority', { required: true })}
+                className=" w-full px-6 py-3 text-black rounded-lg"
+                name="priority"
+                id=""
+                defaultValue="default"
+              >
+                <option disabled value="default"></option>
+                <option value="Low">Low</option>
+                <option value="Moderate">Moderate </option>
+                <option value="High">High</option>
+              </select>
+              {errors.occupation && (
+                <span className="text-red-400">Priority is required</span>
+              )}
+            </div>
 
-              <div className="form-control mt-6">
-                <button className="btn btn-primary" type="submit">
-                  Task Add
-                </button>
-              </div>
-            </form>
-          </div>
+            <div className="form-control mt-6 flex items-center justify-center">
+              <button className="btn btn-primary" type="submit">
+                Task Add
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className=" grid grid-cols-3">
           <div>
-            <div>
-              <h3 className="text-2xl font-semibold">To Do List</h3>
+            <h3 className="text-2xl font-semibold">To Do List</h3>
+            {/* <DragDropContext onDragEnd={handleDragEnd}>
+              <Droppable droppableId="todo">
+                {provided => (
+                  <div {...provided.droppableProps} ref={provided.innerRef}>
+                    {taskData
+                      .filter(item => item.status === 'To-do')
+                      .map((item, index) => (
+                        <Draggable
+                          key={item._id}
+                          draggableId={`todo-${item._id}`}
+                          index={index}
+                        >
+                          {provided => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              className="flex gap-5 px-5 py-4"
+                            >
+                              
+                            </div>
+                          )}
+                        </Draggable>
+                      ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext> */}
+
+            <DragDropContext onDragEnd={handleDragEnd}>
               <Droppable droppableId="todo">
                 {provided => (
                   <div {...provided.droppableProps} ref={provided.innerRef}>
                     {taskData.map((item, index) => (
                       <Draggable
                         key={item._id}
-                        draggableId={item._id}
+                        // draggableId={item._id}
+                        draggableId="todo"
                         index={index}
                       >
                         {provided => (
@@ -215,47 +275,40 @@ const Task = () => {
                             {...provided.dragHandleProps}
                             className="flex gap-5 px-5 py-4 "
                           >
-                            <div className="grid grid-cols-2  bg-green-200">
-                              <div className="flex">
-                                {' '}
-                                <h3>{item.title}</h3>
-                                <h3>{item.description}</h3>
+                            <div
+                              className="grid grid-cols-1 w-full px-4 py-4 rounded-lg"
+                              style={{ backgroundColor: '#0a3d62' }}
+                            >
+                              <div className="flex gap-3">
+                                <h3 className="text-white">{item.title}</h3>
+                                <h3 className="flex-1 text-white">
+                                  {item.description}
+                                </h3>
                               </div>
-                              <div className="flex">
-                                <h3>{item.status}</h3>
-                                <h3>{item.deadlines}</h3>
-                              </div>
-                              <div className="flex">
-                                <button
-                                  onClick={() => handleEdit(item._id)}
-                                  className="btn "
-                                >
-                                  <CiEdit />
-                                </button>
-                                <button
-                                  onClick={() => handleDelete(item._id)}
-                                  className="btn"
-                                >
-                                  <MdDelete />
-                                </button>
+                              <div className="grid grid-cols-2">
+                                <div>
+                                  {' '}
+                                  <h3 className="text-white">{item.status}</h3>
+                                  <h3 className="text-white">
+                                    {item.deadlines}
+                                  </h3>
+                                </div>
+                                <div className=" flex gap-4">
+                                  <button
+                                    onClick={() => handleEdit(item._id)}
+                                    className="btn "
+                                  >
+                                    <CiEdit />
+                                  </button>
+                                  <button
+                                    onClick={() => handleDelete(item._id)}
+                                    className="btn"
+                                  >
+                                    <MdDelete />
+                                  </button>
+                                </div>
                               </div>
                             </div>
-                            {/* <div className="flex">
-                              <h3>{item.status}</h3>
-                              <h3>{item.deadlines}</h3>
-                              <button
-                                onClick={() => handleEdit(item._id)}
-                                className="btn "
-                              >
-                                <CiEdit />
-                              </button>
-                              <button
-                                onClick={() => handleDelete(item._id)}
-                                className="btn"
-                              >
-                                <MdDelete />
-                              </button>
-                            </div> */}
                           </div>
                         )}
                       </Draggable>
@@ -264,51 +317,82 @@ const Task = () => {
                   </div>
                 )}
               </Droppable>
-              ;
-              {/* <div
-              // ref={drop}
-              >
-                {taskData.map(item => (
-                  <div
-                    key={item._id}
-                    // ref={drag}
-                    className=" flex gap-5 px-5 py-4"
-                  >
-                    <div className="grid grid-cols-1">
-                      <h3>{item.title}</h3>
-                      <h3>{item.description}</h3>
-                    </div>
-                    <div className="flex">
-                      <h3>{item.status}</h3>
-                      <h3>{item.deadlines}</h3>
-                      <button
-                        onClick={() => handleEdit(item._id)}
-                        className="btn "
-                      >
-                        <CiEdit />
-                      </button>
-                      <button
-                        onClick={() => handleDelete(item._id)}
-                        className="btn"
-                      >
-                        <MdDelete />
-                      </button>
-                    </div>
+            </DragDropContext>
+          </div>
+          {/* <div>
+            <h3 className="text-2xl font-semibold">Ongoing Task</h3>
+            <DragDropContext onDragEnd={handleDragEnd}>
+              <Droppable droppableId="ongoing">
+                {provided => (
+                  <div {...provided.droppableProps} ref={provided.innerRef}>
+                    {taskData
+                      .filter(item => item.status === 'Ongoing')
+                      .map((item, index) => (
+                        <Draggable
+                          key={item._id}
+                          draggableId={`todo-${item._id}`}
+                          index={index}
+                        >
+                          {provided => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              className="flex gap-5 px-5 py-4"
+                            >
+                              
+                            </div>
+                          )}
+                        </Draggable>
+                      ))}
+                    {provided.placeholder}
                   </div>
-                ))}
-              </div> */}
-            </div>
-            <div>
-              <h3 className="text-2xl font-semibold">Ongoing list</h3>
-            </div>
-            <div>
-              <h3 className="text-2xl font-semibold">Ongoing list</h3>
-            </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </div> */}
+
+          <div>
+            <h3 className="text-2xl font-semibold">Ongoing task</h3>
+          </div>
+
+          {/* <div>
+            <h3 className="text-2xl font-semibold">Complete Task</h3>
+            <DragDropContext onDragEnd={handleDragEnd}>
+              <Droppable droppableId="complete">
+                {provided => (
+                  <div {...provided.droppableProps} ref={provided.innerRef}>
+                    {tasks
+                      .filter(item => item.status === 'Complete')
+                      .map((item, index) => (
+                        <Draggable
+                          key={item._id}
+                          draggableId={`todo-${item._id}`}
+                          index={index}
+                        >
+                          {provided => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              className="flex gap-5 px-5 py-4"
+                            ></div>
+                          )}
+                        </Draggable>
+                      ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </div> */}
+          <div>
+            <h3 className="text-2xl font-semibold">Complete Task</h3>
           </div>
         </div>
-      </DragDropContext>
+      </div>
+      {/* </DragDropContext> */}
     </div>
-    // </DndProvider>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <RouterProvider router={Router} />
-      </AuthProvider>
-    </QueryClientProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <RouterProvider router={Router} />
+    </AuthProvider>
+  </QueryClientProvider>
+  // </React.StrictMode>
 );
